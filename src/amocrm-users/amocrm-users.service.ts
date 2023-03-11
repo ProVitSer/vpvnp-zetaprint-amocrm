41b662926124672180e53e0b6a053c9e33d1ad96
Vitaliy <prokinvs@gmail.com>
2023-03-11 21:26:09 +0100

    ref

--- src/amocrm-users/amocrm-users.service.ts
+++ src/amocrm-users/amocrm-users.service.ts
@@ -1,37 +1,37 @@
-import { PrismaCdrService, PrismaZetaService } from '@app/prisma/prisma.service';
+import { PrismaZetaService } from '@app/prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
 import { AmocrmUsers, Prisma } from '../../prisma/generated/sqlite';
 import { AmocrmUsersServiceInterface, UpdateAmocrmUserData } from './amocrm-users.interface';
 
 @Injectable()
 export class AmocrmUsersService implements AmocrmUsersServiceInterface {
-  constructor(private prisma: PrismaZetaService, private cdr: PrismaCdrService) {}
+  constructor(private zeta: PrismaZetaService) {}
 
   public async createAmocrmUser(data: Prisma.AmocrmUsersCreateInput): Promise<AmocrmUsers> {
-    return this.prisma.amocrmUsers.create({
+    return this.zeta.amocrmUsers.create({
       data,
     });
   }
 
   public async getAmocrmUsers(): Promise<AmocrmUsers[]> {
-    return this.prisma.amocrmUsers.findMany({});
+    return this.zeta.amocrmUsers.findMany({});
   }
 
   public async updateAmocrmUser({ where, data }: UpdateAmocrmUserData): Promise<AmocrmUsers> {
-    return this.prisma.amocrmUsers.update({
+    return this.zeta.amocrmUsers.update({
       data,
       where,
     });
   }
 
   public async deleteAmocrmUser(data: Prisma.AmocrmUsersWhereUniqueInput): Promise<AmocrmUsers> {
-    return this.prisma.amocrmUsers.delete({
+    return this.zeta.amocrmUsers.delete({
       where: data,
     });
   }
 
   public async findAmocrmUser(data: Prisma.AmocrmUsersWhereUniqueInput): Promise<AmocrmUsers | null> {
-    return this.prisma.amocrmUsers.findFirst({
+    return this.zeta.amocrmUsers.findFirst({
       where: data,
     });
   }
